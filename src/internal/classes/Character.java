f13fb068ddccc6827aab6bc2ead86df8c1eec179
Ziocash <31541164+Ziocash@users.noreply.github.com>
2019-04-15 18:58:26 +0200

    Impostazione classe Character
    Inserimento variabili di classe con metodi di get e set delle variabili

--- src/internal/classes/Character.java
+++ src/internal/classes/Character.java
@@ -2,5 +2,23 @@ package internal.classes;
 
 public class Character 
 {
-
+	private String name;
+	private String type;
+	
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getType()
+	{
+		return type;
+	}
+	public void setType(String type)
+	{
+		this.type = type;
+	}
 }
