ed093bf3eea6cd17d08b55bbe98976d841ee2831
Ziocash <Ziocash@users.noreply.github.com>
2019-04-17 10:59:43 +0200

    Implementazione classe CharacterHandler
    Implementazione delle funzioni di gestione dei personaggi

--- src/handlers/CharacterHandler.java
+++ src/handlers/CharacterHandler.java
@@ -1,6 +1,30 @@
 package handlers;
 
-public class CharacterHandler
+import java.util.ArrayList;
+import java.util.List;
+
+public class CharacterHandler<T>
 {
+	private List<T> list;
+	
+	public CharacterHandler()
+	{
+		list = new ArrayList<T>();
+	}
+	
+	public void addCharacter(T character)
+	{
+		list.add(character);
+	}
+	
+	public int countCharacters()
+	{
+		return list.size();
+	}
+	
+	public List<T> getCharactersList()
+	{
+		return list;
+	}
 	
 }
