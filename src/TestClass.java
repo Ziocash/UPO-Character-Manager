88aab8181d43eb0e88d53bf8de308aa0e0a8a933
Ziocash <Ziocash@users.noreply.github.com>
2019-04-17 11:00:24 +0200

    Aggiornamento TestClass
    Inserimento istanze ai nuovi metodi per il test

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,7 @@
 import static java.lang.System.out;
 import org.junit.jupiter.api.Test;
 
+import handlers.CharacterHandler;
 import handlers.FileHandler;
 import internal.classes.BerserkerClass;
 
@@ -12,9 +13,15 @@ class TestClass
 		FileHandler fh = new FileHandler();
 		out.println(fh.getFilePath());
 		out.println(fh.readFile());
+		out.println("Last ID: " + fh.getLastID());
 		BerserkerClass bers = new BerserkerClass(1);
 		bers.setName("Sgravo simulator");
 		out.println(bers.getId()+"|"+bers.getName()+"|"+bers.getConstitution());
+		CharacterHandler<BerserkerClass> ch = new CharacterHandler<BerserkerClass>();
+		ch.addCharacter(bers);
+		out.println(ch.getCharactersList());
+		out.println("Characters in list: " + ch.countCharacters());
+		
 		//out.println("ELLAMADONNA SE SEI SGRAVO");
 	}
 
