6eff6374a9a935e86f0fe9c3b7fbbd4f3c37d6b7
Ziocash <Ziocash@users.noreply.github.com>
2019-04-17 11:00:49 +0200

    Implementazione BerserkerClass
    Miglioramento delle funzioni ed inserimento del moltiplicatore

--- src/internal/classes/BerserkerClass.java
+++ src/internal/classes/BerserkerClass.java
@@ -2,13 +2,18 @@ package internal.classes;
 
 public class BerserkerClass extends Character
 {
-	private final double multiplier = 0.0;
+	private final double multiplier = 12.0;
 	
 	
 	public BerserkerClass(int id) 
 	{
 		super(id);
 		super.setConstitution(541968465);
+	}
+	
+	public double getMultiplier()
+	{
+		return multiplier;
 	}
 
 }
