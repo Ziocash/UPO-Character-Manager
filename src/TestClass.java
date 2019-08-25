76f5da7bee831bedd4615379e0fddc7f0426f2f8
Ziocash <Ziocash@users.noreply.github.com>
2019-08-25 15:50:02 +0200

    Update TestClass.java
    [+] Improved addLine method in CharacterHandler class
    [-] Test correction

--- src/TestClass.java
+++ src/TestClass.java
@@ -17,13 +17,14 @@ class TestClass
 		CharacterHandler ch = new CharacterHandler();
 		//FileHandler test
 		String line="Wizardous witness|1|30.0|200.0|MAGE|ARCANE WARRIOR";
-		out.println(fh.getFilePath());
-		out.println(fh.getDb());
-		fh.addLine(line);
-		line="Wizardous witness|1|40.0|320.0|MAGE|ARCANE WARRIOR";
-		fh.writeLine(line, 3);
-		out.println(fh.writeFile());
-		out.println(fh.getDb());
+		ch.addLine(line);
+		//out.println(fh.getFilePath());
+		//out.println(fh.getDb());
+		//fh.addLine(line);
+		//line="Wizardous witness|1|40.0|320.0|MAGE|ARCANE WARRIOR";
+		//fh.writeLine(line, 3);
+		//out.println(fh.writeFile());
+		//out.println(fh.getDb());
 		//--------------- MainWindow test -----------------------------------
 		MainWindow mw = new MainWindow();
 		mw.show();
